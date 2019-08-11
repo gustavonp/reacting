6cbd00d8a2878064ad248fcbaa770fe01eda43ff
Gustavo Pereira <im.gustavonp@gmail.com>
2019-08-11 02:33:40 -0300

    Adding my React to Git

---
+++ src/index.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PostData from './data/database.json'
+import './index.css';
+
+//DONE -- USAR LOCAL STORAGE DONE
+//DONE -- https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+//DONE -- puxar as funções para fora da class App
+//DONE -- fazer update na tela
+// -- criar um form para dar Add
+//Tentar deixar mais dry
+
+//Começar a planejar o hate-game
+
+function createDB(){
+  localStorage.clear();
+  var obj = PostData;
+  var myData = JSON.stringify(obj);
+  localStorage.setItem('conteudo', myData);
+}
+
+/*
+function removeDb(toRemove){
+  let numero = JSON.parse(localStorage.getItem('conteudo'));
+  for(let i = 0; i < numero.length; i++){
+    if(numero[i] != null){
+      if(numero[i].id == toRemove){
+        delete numero[i];
+      }
+    }
+  }
+  localStorage.setItem('conteudo', JSON.stringify(numero));
+}
+*/
+
+function Poste(props){
+  var heyyou = [];
+  heyyou.push(
+    <div>
+      <h1>{props.title}</h1>
+      <button className="Item" onClick={props.onClick}>
+       'Delete '{props.id}
+      </button>
+    </div>
+  );
+  
+  return(heyyou);
+}
+
+  class PostList extends React.Component {
+    renderItem(item){
+      return(
+        <Poste
+          title={item.title}
+          id={item.id}
+          onClick={(i) => this.removeDb(item.id)}
+          key={item.id}
+        />);
+    }
+
+    renderPost(){
+      var cat = JSON.parse(localStorage.getItem('conteudo'));
+      return(
+        <div>
+        {cat.map((PostDetail, index)=>{
+          if(PostDetail !== null){
+          return(
+            <div className="board-row" key={PostDetail.id}>
+              {this.renderItem(PostDetail)}
+            </div>
+          )
+          }
+        })}
+        </div>
+      );
+    }
+
+
+    addDB(newObj){
+      let numero = JSON.parse(localStorage.getItem('conteudo'));
+      numero.push(newObj);
+      localStorage.setItem('conteudo', JSON.stringify(numero));
+    }
+
+    removeDb(toRemove){
+      let numero = JSON.parse(localStorage.getItem('conteudo'));
+      for(let i = 0; i < numero.length; i++){
+        if(numero[i] != null){
+          if(numero[i].id == toRemove){
+            delete numero[i];
+          }
+        }
+      }
+      localStorage.setItem('conteudo', JSON.stringify(numero));
+      this.setState(this.renderPost()
+
+      );
+    }
+
+    render(){
+      return(
+        <div>
+          {this.renderPost()}
+        </div>
+      )
+    }
+  }
+  
+  class App extends React.Component {
+
+    render() {
+      createDB();
+      var dados = JSON.parse(localStorage.getItem('conteudo'));
+        
+      return (
+        <div className='App'>
+          <h1>Hellos</h1>
+          <PostList
+            cat={dados}
+            onClick={(i) => this.deleteItem(i)}
+          />    
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
---
+++ src/posts/PostList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PostList extends Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello World!</h1>
+        </div>
+      )
+    }
+  }
+
+  export default PostList
